7dcd3e8ae40340cf19496551e4b1862a1b1cadcc
sanjaykrmeena <s.k.meena200@gmail.com>
2023-01-30 21:39:05 +0530

    test

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const fs = require("fs");
+
+const PORT = process.env.PORT;
+
+const home = fs.readFileSync("./index.html", "utf-8");
+
+const server = http.createServer((req, res)=>{
+    if(req.url == "/") {
+        return res.end(home);
+    }
+    else if(req.url == "/about") {
+        return res.end("<h1>ABOUT PAGE</h1>");
+    }
+    else if(req.url == "/contact") {
+        return res.end("<h1>CONTACT PAGE</h1>");
+    }
+    else if(req.url == "/service") {
+        return res.end("<h1>SERVICE PAGE</h1>");
+    }
+    else {
+        return res.end("<h1>404 Page Not Found</h1>");
+    }
+});
+
+server.listen(PORT, ()=> {
+    console.log(`Server is Working`);
+});
